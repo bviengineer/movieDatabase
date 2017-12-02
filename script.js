fc6bbdd34977a841113cb514736d9c6c5fdbbfb1
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-12-02 15:39:03 -0500

    created an array named movies that contains tree objects

---
+++ script.js
@@ -0,0 +1,19 @@
+//Create an array of movie objects that consist of a title, rating and Boolean value based on whether the movie was watched. The information for each movie object will be printed.
+
+var movies = [
+    {
+        title: "Lean On Me",
+        rating: 7.4,
+        hasWatched: true  
+    },
+    {
+        title: "Home Alone",
+        rating: 7.5,
+        hasWatched: true
+    },
+    {
+        title: "Independence Day: Resurgence",
+        rating: 5.3,
+        hasWatched: false
+    }
+];  
